66eb3daa1ec7272ec3d60a141addbcefee57d0ef
Zack Scholl <zack.scholl@gmail.com>
2019-03-22 19:08:47 -0700

    idk

--- midisplitter/main.go
+++ midisplitter/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -17,9 +19,49 @@ import (
 )
 
 func main() {
-	fmt.Println(midi2())
+	midiJSON, err := midiToJSON()
+	if err != nil {
+		fmt.Println(err)
+	}
+	b, _ := json.MarshalIndent(midiJSON, "", " ")
+	fmt.Println("--")
+	fmt.Println(string(b))
+	fmt.Println("--")
+	// fmt.Println(midi2())
 
-	f, err := os.Open("Test.mid")
+	// f, err := os.Open("Test.mid")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer f.Close()
+
+	// rd := smfreader.New(f)
+	// err = rd.ReadHeader()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// header := rd.Header()
+	// var bf bytes.Buffer
+	// wr := smfwriter.New(&bf, smfwriter.TimeFormat(header.TimeFormat), smfwriter.Format(smf.SMF0))
+	// wr.Write(meta.TimeSig{
+	// 	Numerator:                4,
+	// 	Denominator:              4,
+	// 	ClocksPerClick:           24,
+	// 	DemiSemiQuaverPerQuarter: 8,
+	// })
+	// wr.Write(meta.BPM(90))
+	// addMidi("phrase3.mid", &wr)
+	// addMidi("phrase3.mid", &wr)
+	// addMidi("phrase3.mid", &wr)
+	// wr.Write(meta.EndOfTrack)
+	// ioutil.WriteFile("combine.mid", bf.Bytes(), 0644)
+
+}
+
+func midiToJSON() (midiJSON MidiJSON, err error) {
+	bpm := 90.0
+
+	f, err := os.Open("phrase3.mid")
 	if err != nil {
 		panic(err)
 	}
@@ -28,27 +70,84 @@ func main() {
 	rd := smfreader.New(f)
 	err = rd.ReadHeader()
 	if err != nil {
-		panic(err)
+		return
 	}
 	header := rd.Header()
-	var bf bytes.Buffer
-	wr := smfwriter.New(&bf, smfwriter.TimeFormat(header.TimeFormat), smfwriter.Format(smf.SMF0))
-	wr.Write(meta.TimeSig{
-		Numerator:                4,
-		Denominator:              4,
-		ClocksPerClick:           24,
-		DemiSemiQuaverPerQuarter: 8,
-	})
-	wr.Write(meta.BPM(90))
-	addMidi("phrase3.mid", &wr)
-	addMidi("phrase3.mid", &wr)
-	addMidi("phrase3.mid", &wr)
-	wr.Write(meta.EndOfTrack)
-	ioutil.WriteFile("combine.mid", bf.Bytes(), 0644)
+	foo, err := strconv.Atoi(strings.Fields(header.TimeFormat.String())[0])
+	if err != nil {
+		return
+	}
+	ticksPerQuarterNote := uint32(foo)
+	ticksPerSecond := float64(ticksPerQuarterNote) / 60 * bpm
+	midiJSON.Header = Header{
+		Ppq: ticksPerQuarterNote,
+		Tempos: []Tempos{
+			Tempos{
+				Bpm: bpm,
+			},
+		},
+		TimeSignatures: []TimeSignatures{
+			TimeSignatures{
+				TimeSignature: []uint32{4, 4},
+			},
+		},
+	}
 
+	track := Tracks{
+		Channel: 0,
+		Instrument: Instrument{
+			Number: 0,
+			Name:   "acoustic grand piano",
+			Family: "piano",
+		},
+		Notes: []Note{},
+	}
+
+	fmt.Println(ticksPerQuarterNote)
+	var m midi.Message
+
+	curNotes := make(map[string]Note)
+	curTime := 0.0
+	for {
+		m, err = rd.Read()
+		if err != nil {
+			break
+		}
+
+		switch v := m.(type) {
+		case channel.NoteOn:
+			fmt.Printf("%d\ton key: %v velocity: %v\n", rd.Delta(), v.Key(), v.Velocity())
+			curNotes[midiToNote(v.Key())] = Note{
+				Time:     curTime,
+				Name:     midiToNote(v.Key()),
+				Midi:     v.Key(),
+				Velocity: float64(v.Velocity()) / 128,
+			}
+			curTime += float64(rd.Delta()) / ticksPerSecond
+			fmt.Println(curTime)
+		case channel.NoteOff:
+			fmt.Printf("%d\toff key: %v\n", rd.Delta(), v.Key())
+			curTime += float64(rd.Delta()) / ticksPerSecond
+			fmt.Println("duration:", curTime-curNotes[midiToNote(v.Key())].Time)
+		}
+	}
+	midiJSON.Tracks = []Tracks{track}
+
+	if err != smf.ErrFinished {
+		panic("error: " + err.Error())
+	}
+
+	return
 }
 
-func addMidi(fname string, wr *smf.Writer, channum int) (err error) {
+var chromatic = []string{"C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"}
+
+func midiToNote(midiNum uint8) string {
+	midiNumF := float64(midiNum)
+	return fmt.Sprintf("%s%1.0f", chromatic[int(math.Mod(midiNumF, 12))], math.Floor(midiNumF/12.0-1))
+}
+
+func addMidi(fname string, wr *smf.Writer) (err error) {
 	f, err := os.Open(fname)
 	if err != nil {
 		panic(err)
@@ -69,11 +168,6 @@ func addMidi(fname string, wr *smf.Writer, channum int) (err error) {
 	fmt.Println(ticksPerQuarterNote)
 	var m midi.Message
 
-	ch := channel.Channel0
-	if channum == 1 {
-		ch = channel.Channel1
-	}
-
 	for {
 		m, err = rd.Read()
 		if err != nil {
@@ -87,11 +181,11 @@ func addMidi(fname string, wr *smf.Writer, channum int) (err error) {
 		case channel.NoteOn:
 			fmt.Printf("%d\ton key: %v velocity: %v\n", rd.Delta(), v.Key(), v.Velocity())
 			(*wr).SetDelta(rd.Delta())
-			(*wr).Write(ch.NoteOn(v.Key(), v.Velocity()))
+			(*wr).Write(channel.Channel0.NoteOn(v.Key(), v.Velocity()))
 		case channel.NoteOff:
 			fmt.Printf("%d\toff key: %v\n", rd.Delta(), v.Key())
 			(*wr).SetDelta(rd.Delta())
-			(*wr).Write(ch.NoteOff(v.Key()))
+			(*wr).Write(channel.Channel0.NoteOff(v.Key()))
 		}
 
 	}
